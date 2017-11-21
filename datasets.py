1250dee0c37cf6de1dd3204a3cccd42d5bffcfd5
Dan Lindsay <dlindsay72@hotmail.com>
2017-11-21 14:19:57 -0500

    added KNeighborsClassifier

--- datasets.py
+++ datasets.py
@@ -1,5 +1,6 @@
 from sklearn import datasets
 from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
 
 iris = datasets.load_iris()
 
@@ -7,3 +8,10 @@ features = iris.data
 labels = iris.target
 
 features_train, features_test, labels_train, labels_test = train_test_split(features, labels, test_size=.5)
+
+my_classifier = KNeighborsClassifier()
+my_classifier.fit(features_train, labels_train)
+
+prediction = my_classifier.predict(features_test)
+
+print(prediction)
