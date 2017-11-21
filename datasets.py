125aeb55e3a9bd6489a8f3a779e83212d73e6a0f
Dan Lindsay <dlindsay72@hotmail.com>
2017-11-21 14:33:59 -0500

    test successful

--- datasets.py
+++ datasets.py
@@ -1,6 +1,7 @@
 from sklearn import datasets
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score
 
 iris = datasets.load_iris()
 
@@ -14,4 +15,17 @@ my_classifier.fit(features_train, labels_train)
 
 prediction = my_classifier.predict(features_test)
 
-print(prediction)
+print(accuracy_score(labels_test, prediction))
+
+#Versicolor
+iris1 = [[7.1, 2.9, 5.3, 2.4]]
+iris_prediction = my_classifier.predict(iris1)
+
+if iris_prediction[0] == 0:
+    print("Setosa")
+
+if iris_prediction[0] == 1:
+    print("Versicolor")
+
+if iris_prediction[0] == 2:
+    print("Virginica")
